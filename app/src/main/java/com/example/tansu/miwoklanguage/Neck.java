eb74244f247240bc4f4747bd7043ae6da504d7fd
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-01-31 00:43:38 +0530

    remove HackVsit folder

---
+++ app/src/main/java/com/example/tansu/miwoklanguage/Neck.java
@@ -0,0 +1,12 @@
+package com.example.tansu.miwoklanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Neck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
