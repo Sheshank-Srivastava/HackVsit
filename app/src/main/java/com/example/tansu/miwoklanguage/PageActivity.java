2af16fc5e3a12569a0d7aab455e12146a9938263
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-01-30 18:58:24 +0530

    second commit

--- app/src/main/java/com/example/tansu/miwoklanguage/PageActivity.java
+++ app/src/main/java/com/example/tansu/miwoklanguage/PageActivity.java
@@ -2,12 +2,32 @@ package com.example.tansu.miwoklanguage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.felipecsl.gifimageview.library.GifImageView;
 
 public class PageActivity extends AppCompatActivity {
-
+    GifImageView image;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pages);
+        /**
+         * hello git status
+         *
+         */
+        image = (GifImageView) findViewById(R.id.gif_image);
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                image.startAnimation();
+            }
+        });
+
     }
 }
